61f906284ecb26795f2ea5c2200a4277dd08ede3
bhaskarkrp <bhaskarkumar0022@gmail.com>
2022-04-05 18:18:20 +0530

    Updated Protfolio

--- src/App.js
+++ src/App.js
@@ -18,14 +18,7 @@ import "./App.css";
 function App() {
   const { theme } = useContext(ThemeContext);
 
-  console.log(
-    "%cDEVELOPER PORTFOLIO",
-    `color:${theme.primary}; font-size:50px`
-  );
-  console.log(
-    "%chttps://github.com/hhhrrrttt222111/developer-portfolio",
-    `color:${theme.tertiary}; font-size:20px`
-  );
+  console.log("%cBHASKAR KUMAR", `color:${theme.primary}; font-size:50px`);
   // console.log = console.warn = console.error = () => {};
 
   return (
